27d35264328673cbd9f53aa4beeb2f1cd7c698cf
Josh Crowder <josh@seriousfox.co.uk>
2012-09-24 19:10:53 +0100

    Can't quite figure this out. Needs more time.

--- lib/freeagent/client.rb
+++ lib/freeagent/client.rb
@@ -56,7 +56,17 @@ module FreeAgent
     end
 
     def get(path, params={})
-      request(:get, "#{Client.site}#{path}", :params => params).parsed
+      response = request(:get, "#{Client.site}#{path}", :params => params)
+      response2 = request(:get, "https://api.freeagent.com/v2/bank_transaction_explanations?page=2&per_page=100&bank_account=92599", :params => params)
+      # 
+      # response = MultiJson.load(response.body)
+      # response2 = MultiJson.load(response2.body)
+      # flat_array = [response,response2].map(&:to_a).flatten
+      # # Hash[*flat_array]
+      # puts flat_array
+      puts response.parsed['bank_transaction_explanations'] << response2.parsed['bank_transaction_explanations']
+      puts '--------------'
+      response.parsed
     end
 
     def post(path, data={})
@@ -70,28 +80,31 @@ module FreeAgent
     def delete(path, data={})
       request(:delete, "#{Client.site}#{path}", :data => data).parsed
     end
-
+    
   private
 
     def request(method, path, options = {})
       if @access_token
+        options[:params].merge!(:per_page => 100)
         options[:body] = MultiJson.encode(options[:data]) unless options[:data].nil?
         @access_token.send(method, path, options)
-        puts @access_token.inspect
-        puts links(@access_token)
       else
         raise FreeAgent::ClientError.new('Access Token not set')
       end
+      
     rescue OAuth2::Error => error
       api_error = FreeAgent::ApiError.new(error.response)
       puts api_error if FreeAgent.debug
       raise api_error
     end
     
+    
+    # {"server"=>"nginx/1.0.14", "date"=>"Mon, 24 Sep 2012 14:08:08 GMT", "content-type"=>"application/json; charset=utf-8", "transfer-encoding"=>"chunked", "connection"=>"close", "status"=>"200 OK", "link"=>"<https://api.freeagent.com/v2/bank_transaction_explanations?page=2&per_page=25&bank_account=92599>; rel='next', <https://api.freeagent.com/v2/bank_transaction_explanations?page=31&per_page=25&bank_account=92599>; rel='last'", "etag"=>"\"32b0959ea1a5327ac34fcce26d2d651b\"", "last-modified"=>"Thu, 19 Jan 2012 14:32:09 GMT", "cache-control"=>"max-age=0, private, must-revalidate", "x-ua-compatible"=>"IE=Edge,chrome=1", "x-runtime"=>"0.616217", "x-rev"=>"c265581", "x-host"=>"web4"}
+
     def links(response)
-      links = ( response.headers["Link"] || "" ).split(', ').map do |link|
-        url, type = link.match(/<(.*?)>; rel="(\w+)"/).captures
-        [ type, url ]
+      links = ( response.headers["link"] || "" ).split(', ').map do |link|
+        url, type = link.match(/<(.*?)>; rel='(\w+)'/).captures
+        [type,url]
       end
       Hash[ *links.flatten ]
     end
